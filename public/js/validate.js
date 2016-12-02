6d7b43edf42b374f78ac702aae9e75438937cb16
Tobias Thurnreiter <thurnreiter@gmail.com>
2016-12-02 23:12:46 +0100

    added frontend validation on signup.hbs and login.hbs

---
+++ public/js/validate.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  $('.ui.form.signup').form({
+    fields: {
+      input1: {
+        identifier: 'name',
+        rules: [{
+          type: "empty",
+          prompt: "Please enter your name"
+        }]
+      },
+      input2: {
+        identifier: 'email',
+        rules: [{
+          type: "email",
+          prompt: "Please enter a valid Email"
+        },
+        ]
+      },
+      input3: {
+        identifier: 'password',
+        rules: [{
+          type: 'minLength[6]',
+          prompt: 'Your password must be at least {ruleValue} characters'
+        }]
+      }
+    },
+  });
+  $('.ui.form.login').form({
+    fields: {
+      input1: {
+        identifier: 'email',
+        rules: [{
+          type: "email",
+          prompt: "Please enter a valid Email"
+        },
+        ]
+      },
+    },
+  });
+});
